9823757105e45ad427f2147d20c7989cc9285aad
“yangguochao <326793394@qq.com>
2018-05-11 22:03:54 +0800

    commit demo1

---
+++ koa2_demo1/app.js
@@ -0,0 +1,8 @@
+const Koa = require('koa')
+const app = new Koa()
+app.use( async ( ctx ) => {
+  ctx.response.type = 'text/html';
+  ctx.response.body = '<h1>Hello, koa2!</h1>';
+})
+app.listen(3000)
+console.log('[demo] start-quick is starting at port 3000')
